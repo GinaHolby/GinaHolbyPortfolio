87528369734ec46734a071453111d2cc200389d6
GinaHolby <gina.s.holby@gmail.com>
2023-10-30 13:25:43 +0100

    j

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,17 @@
-export const schemaTypes = []
+
+
+// schemas/index.js
+
+
+import arrayContent from './arrayContent'
+import blockContent from './blockContent'
+import project from './project'
+
+
+export const schemaTypes = [
+
+    project,
+    arrayContent,
+    blockContent
+
+]
